19fe392c49c940929098bb3268c566618a8eae7d
J-Groesbeck <jacob.groesbeck@icloud.com>
2024-05-22 14:59:59 -0400

    nav working on homepage, broken on product page but there. added images

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 const productsInfo = [{
     name: 'Wooden Airplane',
     price: 19.99,
-    image: 'product-imgs/placeholder.png',
+    image: 'product-imgs/plane1.png',
     description: `Soar through imaginative skies with this classic wooden airplane. Handcrafted from sustainable Baltic birch wood with a safe, natural harvest finish and a spinning propeller. Measures 3.5"H x 7"L x 7"W.'`,
     ages: 'Toddlers'
 },
 {
     name: 'Wooden Train Set',
     price: 34.99,
-    image: 'product-imgs/placeholder.png',
+    image: 'product-imgs/train1.png',
     description: `Embark on a charming journey with this beautiful handcrafted wooden train set. Engine and three interchangeable cars boast intricate details made from real beech wood. Large size (84cm L x 11cm H x 13cm W) with moving wheels and a fully ecological design.`,
     ages: '3+'
 },
 {
     name: 'Wooden Boat',
     price: 14.99,
-    image: 'product-imgs/placeholder.png',
+    image: 'product-imgs/boat1.png',
     description: `Set sail for bathtub adventures with this adorable wooden boat. Made from solid Maine white pine, this handcrafted toy floats and features rounded edges for safety. Includes two peg "lobster people." Size: 10.5"W x 3.5"H.`,
     ages: 'All Ages'
 },
 {
     name: 'Wooden Block Set',
     price: 99.99,
-    image: 'product-imgs/placeholder.png',
+    image: 'product-imgs/block-set1.png',
     description: `Build creativity and imagination with this high-quality, 72-piece block set. Made from naturally finished and smooth-sanded hardwood blocks, this set comes in a convenient wooden storage crate (13” L x 12” W x 2” H).`,
     ages: '3+'
 },
 {
     name: 'Wooden Car',
-    price: 14.99,
-    image: 'product-imgs/placeholder.png',
+    price: 16.99,
+    image: 'product-imgs/car1.png',
     description: `This heirloom-quality wooden car is a timeless treasure. Handcrafted from domestic and exotic hardwoods with a clear lacquer finish, this unique car will inspire generations of imaginative play. Please note potential choking hazards for small children.`,
     ages: 'All Ages'
 }
@@ -43,7 +43,7 @@ function generateProductCards() {
         card.classList.add('col-12', 'col-md-6', 'col-lg-4', 'mb-1')
 
         card.innerHTML = `
-        <div class="card text-center overflow-x-hidden">
+        <div id="card${i}" class="card text-center overflow-x-hidden h-100">
             <div class="card-header">
             ${product.name} (${product.ages})
             </div>
@@ -62,7 +62,7 @@ function generateProductCards() {
             </div>
         </div>
         <div class="modal fade" id="modal${i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
+            <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h1 class="modal-title fs-5" id="exampleModalLabel">${product.name}</h1>
@@ -74,10 +74,18 @@ function generateProductCards() {
                                 <div class="col-8">
                                     <img src="${product.image}" class="w-100">
                                 </div>
-                                <div class="col-4>
-                                    <button class="btn btn-primary w-100" onclick="addToCart(${i})">
-                                        Add To Cart
-                                    </button>
+                                <div class="col-4">
+                                    <div class="row">
+                                        <div class="col-12 text-end">
+                                            <h3 class="mb-0">${product.name}<br>$${product.price}</h3>
+                                        </div>
+                                        <div class="col-12 my-2">
+                                            <button class="btn btn-primary w-100" onclick="addToCart(${i})">Add To Cart</button>
+                                        </div>
+                                        <div class="col-12">
+                                            <h5>${product.description}</h5>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -121,7 +129,7 @@ function displayCart() {
     let i = 0
     cartArray.forEach(item => {
         const card = document.createElement('div')
-        card.classList.add('col-12','mb-1')
+        card.classList.add('col-12', 'mb-1')
 
         card.innerHTML = `
         <div class="card" style="width: 18rem;">
@@ -163,8 +171,8 @@ function displayCart() {
 
 function updatePrice(itemNumber) {
     let itemAmount = document.getElementById(`itemAmount${itemNumber}`).value
-    document.getElementById(`itemPrice${itemNumber}`).innerHTML =  `$${cartArray[itemNumber].price * itemAmount}`
-    totalPrice.innerHTML = `Total: $${getTotalPrice()}` 
+    document.getElementById(`itemPrice${itemNumber}`).innerHTML = `$${cartArray[itemNumber].price * itemAmount}`
+    totalPrice.innerHTML = `Total: $${getTotalPrice()}`
 }
 
 function getTotalPrice() {
@@ -176,52 +184,3 @@ function getTotalPrice() {
     });
     return totalPrice
 }
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var header = document.getElementById('myHeader');
-	  var page = document.getElementById('page');
-    var openMenuButton = document.getElementById('openmenu');
-
-    window.addEventListener('scroll', function() {
-				page.classList.remove('menuopen');
-        if (window.scrollY >= 100) {
-            header.classList.add('sticky');
-        } else {
-            header.classList.remove('sticky');
-        }
-    });
-
-    // Event listener to remove the sticky class when the button is clicked
-    openMenuButton.addEventListener('click', function() {
-        header.classList.remove('sticky');
-				page.classList.add('menuopen');
-                console.log("bruh")
-
-    });
-	
-	var links = document.querySelectorAll('a[href^="#"]');
-
-    links.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            // Prevent the default action
-            event.preventDefault();
-
-            // Get the target element
-            var targetId = this.getAttribute('href');
-            var targetElement = document.querySelector(targetId);
-
-            // Smooth scroll to target
-            if (targetElement) {
-                targetElement.scrollIntoView({
-                    behavior: 'smooth'
-                });
-            }
-        });
-    });
-});
-
-
-console.log("hello")
